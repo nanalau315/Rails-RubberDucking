909292d578281af5ffbfdd23c31e231af1701811
Nana Lau <nanalau315@hotmail.com>
2020-12-17 01:11:10 -0500

    Completed Lab

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -1,7 +1,45 @@
 class DucksController < ApplicationController
-
+  # ducks[:index, :show, :new, :create, :edit, :update]
+ 
   def index
     @ducks = Duck.all
   end
 
+  def show
+    @duck = Duck.find(params[:id])
+  end
+
+  def new
+    @duck = Duck.new
+    @students = Student.all
+  end
+
+  def create
+    @duck = Duck.create(duck_params(:name, :description, :student_id))
+    if @duck.valid?
+      redirect_to duck_path(@duck)
+    else
+      flash[:errors] = @duck.errors.full_messages
+      redirect_to new_duck_path
+    end
+  end
+
+  def edit
+    @duck = Duck.find(params[:id])
+  end
+
+  def update
+    @duck = Duck.find(params[:id])
+    if @duck.update(duck_params(:name, :description, :student_id))
+      redirect_to duck_path(@duck)
+    else
+      flash[:errors] = @duck.errors.full_messages
+      redirect_to edit_duck_path
+    end
+  end
+
+  private
+  def duck_params(*args)
+    params.require(:duck).permit(*args)
+  end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,44 @@
 class StudentsController < ApplicationController
-
+ 
+  # students [:index, :show, :new, :create, :edit, :update]
   def index
     @students = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(student_params(:name, :mod))
+    if @student.valid?
+      redirect_to student_path(@student)
+    else
+      flash[:errors] = @student.errors.full_messages
+      redirect_to new_student_path
+    end
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    if @student.update(student_params(:name, :mod))
+      redirect_to student_path(@student)
+    else
+      flash[:errors] = @student.errors.full_messages
+      redirect_to edit_student_path(@student)
+    end
+  end
+
+  private
+  def student_params(*args)
+    params.require(:student).permit(*args)
   end
 
 end
